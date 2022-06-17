0126ba694288fbf4c9f32c5d556a380be213a7dc
John Eskandar <john.eskandar@espace.com.eg>
2022-06-17 14:46:35 +0200

    add more content to homepage

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!' +
+    'added an update')
 })
 
 app.listen(port, () => {
